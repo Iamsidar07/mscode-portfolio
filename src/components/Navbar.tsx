060ea2f690646dcd3d27efb6a7401d070261b438
iamsidar07 <ms8460149@gmail.com>
2023-11-01 11:02:06 +0530

    fixed mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,13 +37,13 @@ const Navbar = () => {
 
                 {
                     isDropdownOpen &&
-                    <div className='flex flex-col md:hidden  gap-4 absolute top-16 right-4 min-w-[200px] py-4  border border-[#1d31619a] rounded-md z-50 shadow-sm bg-gradient-radial from-[#0f172a] to-transparent'>
+                    <div className='flex flex-col md:hidden  gap-4 absolute top-16 right-0 min-w-[200px] py-4  border border-gray-800 rounded-md z-50 shadow text-center bg-gray-900'>
                         {
-                            navLinks.map((link, index) => (<Link onClick={() => setIsDropdownOpen(false)} className='pl-4 border-b border-b-[#1d31619a] last:border-none py-1' href={`/#${link.id}`} key={index}>
+                            navLinks.map((link, index) => (<Link onClick={() => setIsDropdownOpen(false)} className='pl-4 border-b border-gray-800  py-1' href={`/#${link.id}`} key={index}>
                                 {link.title}
                             </Link>))
                         }
-                        <Link onClick={() => setIsDropdownOpen(false)} target='_blank' className='pl-4 border-b border-b-[#1d31619a] last:border-none py-1' href={`https://personal-dev-blog-nu.vercel.app/`}>
+                        <Link onClick={() => setIsDropdownOpen(false)} target='_blank' className='pl-4 border-b border-gray-800 last:border-none' href={`https://personal-dev-blog-nu.vercel.app/`}>
                             Blog
                         </Link>
                     </div>
