0b0a6d7bd7fa806119b0fca6403e20199ed57fbd
iamsidar07 <ms8460149@gmail.com>
2024-04-25 19:13:44 +0530

    add horizontal scroll for desktop

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -22,7 +22,8 @@ const ProjectCard = ({
   tags,
 }: ProjectCardProps) => {
   return (
-    <div className="max-w-[350px] lg:max-w-[450px] border border-gray-800 flex flex-col rounded-lg group overflow-hidden ">
+    <div className="min-w-screen border border-gray-800 flex flex-col rounded-lg group card overflow-hidden relative backdrop-blur-xl ring ring-teal-950 group">
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-teal-950 z-10 h-0 group-hover:h-full transition-all" />
       <Link
         href={appURL}
         target="_blank"
@@ -36,15 +37,27 @@ const ProjectCard = ({
           className="object-cover"
         />
       </Link>
-      <div className="p-4">
-        <Link href={appURL}>
-          {" "}
-          <h1 className="text-white capitalize text-lg lg:text-2xl group-hover:underline transition-all duration-200 ease-linear tracking-wider">
-            {title}
-          </h1>
+
+      <div className="p-4 absolute w-full -bottom-1/2 group-hover:bottom-1/3 group-hover:translate-y-1/3 flex flex-col items-center justify-center z-50 transition-transform">
+        <Link
+          href={appURL}
+          target={"_blank"}
+          className="w-20 h-20 rounded-full bg-gradient-to-br from-indigo-400 to-indigo-900 overflow-hidden p-2"
+        >
+          <Image
+            src={"/link.png"}
+            width={100}
+            height={100}
+            alt="view more"
+            className="w-full h-full object-contain"
+          />
         </Link>
 
-        <p className="mt-2 text-left text-gray-300">{description}</p>
+        <h1 className="text-white capitalize text-lg lg:text-2xl tracking-wider">
+          {title}
+        </h1>
+
+        <p className="mt-2 text-gray-300">{description}</p>
         <div className="flex items-center gap-1 flex-wrap mt-2.5">
           {tags?.map((tag, i) => (
             <span key={i} className={`${tag.color}`}>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,32 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import { projects } from "@/constants";
 import { square, twoline } from "@/assets";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
 const Projects = () => {
+  const container = useRef<HTMLDivElement | null>(null);
+  useGSAP(() => {
+    const cards = gsap.utils.toArray(".card");
+    gsap.to(cards, {
+      xPercent: -100 * (cards.length - 1),
+      scrollTrigger: {
+        trigger: "#work",
+        start: "top top-=100",
+        end: "+=" + container?.current?.offsetWidth,
+        scrub: 0.3,
+        pin: true,
+      },
+    });
+  }, []);
   return (
-    <section id={"work"} className="relative  py-24 border-y border-gray-900">
+    <section
+      ref={container}
+      id={"work"}
+      className={`relative py-24 border-y border-gray-900 overflow-x-hidden h-[${projects.length * 100}vh] `}
+    >
       <div className="absolute top-12 -left-6 w-24 lg:w-36 h-24 lg:h-36 z-20">
         <Image src={twoline} alt="twoline" fill className="object-contain " />
       </div>
@@ -20,31 +41,28 @@ const Projects = () => {
 
       <div className="bg-violet-700 w-[300px] h-[300px] absolute blur-3xl rounded-full -z-50 filter bg-opacity-20 top-1/2 right-1/3" />
 
-      <div className="flex flex-col items-center justify-center max-w-[1440px] mx-auto relative">
-        <h2 className="text-2xl lg:text-7xl font-medium text-white">
-          Manoj&apos;s Portfolio
-        </h2>
+      <h2 className="text-2xl lg:text-7xl font-medium text-white text-center">
+        Manoj&apos;s Portfolio
+      </h2>
 
-        <div className="bg-violet-700 w-[300px] h-[300px] absolute blur-3xl rounded-full -z-50 filter bg-opacity-20 -top-24 right-1/3" />
-        <p className="max-w-md text-center text-sm lg:text-lg my-6 text-white">
-          Web apps that I have worked on.
-        </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4 mt-12">
-          {projects.map((project, i) => (
-            <ProjectCard
-              title={project.name}
-              description={project.description}
-              image={project.image}
-              appURL={project.liverUrl}
-              key={i}
-              tags={project.tags}
-            />
-          ))}
-        </div>
+      <div className="bg-violet-700 w-[300px] h-[300px] absolute blur-3xl rounded-full -z-50 filter bg-opacity-20 -top-24 right-1/3" />
+      <p className=" text-center text-sm lg:text-lg my-6 text-white">
+        Web apps that I have worked on.
+      </p>
+      <div className="flex items-center justify-center flex-nowrap space-x-10   w-[900vh] px-20">
+        {projects.map((project, i) => (
+          <ProjectCard
+            title={project.name}
+            description={project.description}
+            image={project.image}
+            appURL={project.liverUrl}
+            key={i}
+            tags={project.tags}
+          />
+        ))}
       </div>
     </section>
   );
 };
 
 export default Projects;
-
