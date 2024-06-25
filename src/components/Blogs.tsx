ecdb60c24fd999c8e2e556b726d050717a4a2857
Manoj Kumar <ms8460149@gmail.com>
2024-06-25 20:16:56 +0530

    fix blog path

--- src/components/Blogs.tsx
+++ src/components/Blogs.tsx
@@ -7,7 +7,7 @@ const Blogs = ({ amount = 5 }) => {
     <div className="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2  gap-4 md:gap-10 py-12 px-4">
       {allPosts?.slice(0, amount).map((post) => (
         <a
-          href={`/blogs/${post.slug}`}
+          href={`/blogs${post.slug}`}
           key={post._id}
           className="border rounded border-gray-800 overflow-hidden"
         >
